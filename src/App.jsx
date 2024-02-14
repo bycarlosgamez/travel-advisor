456c3cffb46973ee8b1cab9d21ae1e79031d26b0
Carlos Gámez <hardpx@gmail.com>
2024-02-13 18:33:39 -0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Details from './components/Details/Details';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Map />
+      <List />
+      <Details />
+    </>
+  );
+};
+
+export default App;
